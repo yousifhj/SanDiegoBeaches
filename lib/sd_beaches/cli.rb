41eee43f83b16adc804ce3ec7d7d0662687f593a
Huda Yousif <hjdoski@gmail.com>
2019-10-30 02:41:20 +0000

    split string

--- lib/sd_beaches/cli.rb
+++ lib/sd_beaches/cli.rb
@@ -1,5 +1,11 @@
 class Cli
     
+    def run 
+      welcome
+      responce
+    end 
+    
+    
     def run
         puts "Welcome to San Diego's Beaches and Bays"
         puts "70 Miles of Glorious Coastline Signature is San Diego's Signature Attraction "
@@ -11,7 +17,12 @@ class Cli
         html = open("https://www.sandiego.org/explore/things-to-do/beaches-bays.aspx")
         doc = Nokogiri::HTML(html)
         
-        doc.css(".areas-of-interest_gallery").each do |beach|
+        doc.css(".areas-of-interest__text").each do |beach|
+          beach.css
+          
+          beach = index + 1 
+          
+          string.split(/(?<!\s)(?=[A-Z])/)
         binding.pry 
       end 
       
