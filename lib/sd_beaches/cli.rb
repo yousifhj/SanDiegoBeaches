dfa039a67b53d8bcaf7e567da212cdfa2234165e
Huda Yousif <hjdoski@gmail.com>
2019-10-31 22:42:42 +0000

    update cli

--- lib/sd_beaches/cli.rb
+++ lib/sd_beaches/cli.rb
@@ -1,48 +1,49 @@
 class Cli
   
+  # BASE_PATH = "https://www.sandiego.org/explore/things-to-do/beaches-bays.aspx"
+  
   def run 
     greeting
-    first_option
+    first_question
     Scraper.scrape_beaches
-    call
   end 
     
-    def call 
-      user_input = menu
-      if user_input == "exit" || user_input == "n"
-        goodbye
-        return
-      else 
-        print_beaches
-        choose_beach
-      end 
-    end 
-
-    def greeting
-      puts "-----------------------------------------------------------------------------"
-      puts "Welcome to San Diego's Beaches and Bays"
-      puts "70 Miles of Glorious Coastline Signature is San Diego's Signature Attraction "
-      puts "These are the top beaches in San Diego"
-      puts "-----------------------------------------------------------------------------"
-    end 
-    
-    def first_option
-      puts "Would you like to see the top beaches in San Diego  (y/n)?"
-      
-      reply = gets.chomp 
-      
-      case reply
-      when "y"
-        print_beaches
-      when "n"
-        puts "We hope to see you soon!"
+  def call 
+    user_input = menu      
+    if user_input == "exit" || user_input == "n"
+      goodbye
       return
-    end
+    else 
+      print_beaches
+      choose_beach
+    end 
   end 
+
+  def greeting
+    puts "-----------------------------------------------------------------------------"
+    puts "Welcome to San Diego's Beaches and Bays"
+    puts "70 Miles of Glorious Coastline Signature is San Diego's Signature Attraction "
+    puts "These are the top beaches in San Diego"
+    puts "-----------------------------------------------------------------------------"
+  end 
+    
+  def first_question
+    puts "Would you like to see the top beaches in San Diego  (y/n)?"
+      
+    reply = gets.chomp 
+      
+    case reply
+    when "y"
+      print_beaches
+    when "n"
+      puts "We hope to see you soon!"
+    return
+  end
+  
     
     def print_beaches     
-       Beach.all.each.with_index(1) do |beach, index|
-         puts "#{index}. #{beach.name}"
+      Beach.all.each.with_index(1) do |beach, index|
+      puts "#{index}. #{beach.name}"
       end 
     end 
       
@@ -50,34 +51,18 @@ class Cli
       puts "Select the beach you want to learn more about."
       index = gets.strip.to_i - 1 
       beach = Beach.all[index]
-      # if input_valid?(index)
-      # self.description(beach)
-      # self.what_to_know(beach)
-      # self.what_to_know(beach)
     end 
-      
-      
-        # while input != 'exit' do 
-        #   beach = Beach.all[input.to_i - 1]
-          
-      #     Scraper.scrape_beach_deatils(beach)
-      #     print_beach(beach)
-      #     puts " "
-      #     puts "Would you like to see another beach?"
-      #     puts "If so, select the number of the beach you want to see further details or type 'exit' to exit"
-         # input = gets.strip.downcase
        
-       
-      def menu
-        input = gets.strip.downcase
-        return input 
-      end
+    def menu
+      input = gets.strip.downcase
+      return input 
+    end
     
-      def print_beach(beach)
-        puts "#{beach.name}"
-        puts "#{beach.what_to_love}"
-        puts "#{beach.what_to_know}"
-      end 
+    def print_beach(beach)
+      puts "#{beach.name}"
+      puts "#{beach.what_to_love}"
+      puts "#{beach.what_to_know}"
+    end 
      
     def input_valid?(input)
       input == input.to_i
@@ -90,7 +75,21 @@ class Cli
     def come_again
       puts "Hope to see you soon!"
     end 
-    
   end 
-
 end
+end 
+
+   # if input_valid?(index)
+        # self.description(beach)
+        # self.what_to_know(beach)
+        # self.what_to_know(beach)
+        # while input != 'exit' do 
+        #   beach = Beach.all[input.to_i - 1]
+          
+      #     Scraper.scrape_beach_deatils(beach)
+      #     print_beach(beach)
+      #     puts " "
+      #     puts "Would you like to see another beach?"
+      #     puts "If so, select the number of the beach you want to see further details or type 'exit' to exit"
+         # input = gets.strip.downcase
+       
