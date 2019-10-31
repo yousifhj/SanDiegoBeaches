510f1cb3f8b050f4de7ed18d011d5e393bf0c160
Huda Yousif <hjdoski@gmail.com>
2019-10-31 06:37:22 +0000

    update scrape

--- lib/sd_beaches/scraper.rb
+++ lib/sd_beaches/scraper.rb
@@ -7,14 +7,17 @@ class Scraper
       beach = Beach.new
       beach.name = b.css(".areas-of-interest__text").text.strip
       beach.url = b.attribute("href").value
+      #binding.pry
     end 
   end 
   
   def self.scrape_beach_deatils(beach)
-      binding.pry
-    html = open("https://www.sandiego.org/explore/things-to-do/beaches-bays/#{beach.url}")
-    doc = Nokogiri::HTML(html)
-  end 
+   html = open("https://www.sandiego.org/explore/things-to-do/beaches-bays.aspx")
+   doc = Nokogiri::HTML(html)
+   doc.css(".areas-of-interest__gallery-entry").attribute("href").value
+  
+   #binding.pry
+  end   
   
 end 
 
