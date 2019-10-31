2cb15fd59004b6a37e9435b1bca4ce72af0c4610
Huda Yousif <hjdoski@gmail.com>
2019-10-31 07:39:35 +0000

    update scraper deatails

--- lib/sd_beaches/scraper.rb
+++ lib/sd_beaches/scraper.rb
@@ -3,20 +3,25 @@ class Scraper
   def self.scrape_beaches
     html = open("https://www.sandiego.org/explore/things-to-do/beaches-bays.aspx")
     doc = Nokogiri::HTML(html)
-    doc.css(".areas-of-interest__gallery-entry").each do |b|
-      beach = Beach.new
-      beach.name = b.css(".areas-of-interest__text").text.strip
-      beach.url = b.attribute("href").value
-      #binding.pry
-    end 
+   
+      doc.css(".areas-of-interest__gallery-entry").each do |b|
+        beach = Beach.new
+        beach.name = b.css(".areas-of-interest__text").text.strip
+        beach.url = b.attribute("href").value
+        #binding.pry
+      end 
+    
   end 
   
   def self.scrape_beach_deatils(beach)
    html = open("https://www.sandiego.org/explore/things-to-do/beaches-bays.aspx")
    doc = Nokogiri::HTML(html)
-   doc.css(".areas-of-interest__gallery-entry").attribute("href").value
-  
-   #binding.pry
+   container = doc.css(".areas-of-interest__gallery-entry").attribute("href").value
+   container.each do |link|
+     if link.include?("What to Love")
+       
+   end 
+   
   end   
   
 end 
