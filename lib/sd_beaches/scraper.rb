8a9b3aaa1aee578aedf91c09ce44b140ced93c2d
Huda Yousif <hjdoski@gmail.com>
2019-11-01 15:39:31 +0000

    update validations

--- lib/sd_beaches/scraper.rb
+++ lib/sd_beaches/scraper.rb
@@ -1,21 +1,31 @@
 class Scraper
 
   def self.scrape_beaches
+    
     html = open("https://www.sandiego.org/explore/things-to-do/beaches-bays.aspx")
+    
     doc = Nokogiri::HTML(html)
    
       doc.css(".areas-of-interest__gallery-entry").each do |b|
+        
         beach = Beach.new
+        
         #binding.pry
+        
         beach.name = b.css(".areas-of-interest__text").text.strip
+        
         beach.url = b.attribute("href").value
+        
       end 
     
   end 
   
   def self.scrape_beach_deatils(beach)
+    
     html = open(beach.url)
+    
     doc = Nokogiri::HTML(html)
+    
     beaches = {}
  
      container = doc.css("attributes__info")
